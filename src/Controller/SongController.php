b334fb02f06326ab2d5980984eda5361c0104421
DominikWolos <dominik.wolos@gmail.com>
2023-09-16 19:02:11 +0200

    before webpack

---
+++ src/Controller/SongController.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class SongController extends AbstractController
+{
+    #[Route(path: '/api/songs/{id<\d+>}', name: 'app_api_song_show', methods: ['GET'])]
+    public function getSong(int $id, LoggerInterface $logger): Response
+    {
+        $song = [
+            'id' => $id,
+            'name' => 'Waterfalls',
+            'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
+        ];
+
+        $logger->info('Song {id} was viewed', ['id' => $id]);
+
+        return $this->json($song);
+    }
+}
--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -4,26 +4,41 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-final class VinylController
+final class VinylController extends AbstractController
 {
-    #[Route(path: '/', name: 'homepage', methods: ['GET'])]
+    #[Route(path: '/', name: 'app_homepage', methods: ['GET'])]
     public function homepage(): Response
     {
-        return new Response('Title: PB and Jams');
+        $tracks = [
+            ['artist' => 'The Beatles ', 'song' => ' Abbey Road',],
+            ['artist' => 'The Beatles ', 'song' => ' Sgt. Pepper\'s Lonely Hearts Club Band',],
+            ['artist' => 'The Beatles ', 'song' => ' Revolver',],
+            ['artist' => 'The Beatles ', 'song' => ' Rubber Soul',],
+            ['artist' => 'The Beatles ', 'song' => ' The Beatles (White Album)',],
+            ['artist' => 'The Beatles ', 'song' => ' Magical Mystery Tour',],
+            ['artist' => 'The Beatles ', 'song' => ' Let It Be',],
+            ['artist' => 'The Beatles ', 'song' => ' Please Please Me',],
+            ['artist' => 'The Beatles ', 'song' => ' With The Beatles',],
+            ['artist' => 'The Beatles ', 'song' => ' A Hard Day\'s Night',],
+        ];
+
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks,
+        ]);
     }
 
-    #[Route(path: '/browse/{slug}', name: 'browse', methods: ['GET'])]
+    #[Route(path: '/browse/{slug}', name: 'app_vinyl_browse', methods: ['GET'])]
     public function browse(string $slug = null): Response
     {
-        if (null === $slug) {
-            return new Response('All Vinyls');
-        }
+        $genre = $slug ? ucwords(str_replace('-', ' ', $slug)) : null;
 
-        $title = ucwords(str_replace('-', ' ', $slug));
-
-        return new Response("Genre: $title");
+        return $this->render('vinyl/browse.html.twig', [
+            'genre' => $genre,
+        ]);
     }
 }
